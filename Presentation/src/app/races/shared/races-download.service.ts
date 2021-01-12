09627aa68e4b361e334c165d2a1fd6f44dca620f
M-arc-O <m.trapman@gmail.com>
2021-01-12 13:14:49 +0100

    Updated to angular 11

--- Presentation/src/app/races/shared/races-download.service.ts
+++ Presentation/src/app/races/shared/races-download.service.ts
@@ -1,6 +1,5 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ResponseContentType } from '@angular/http';
 import { Observable } from 'rxjs';
 import { ConfigurationService, ServiceBase } from 'src/app/shared';
 
@@ -14,17 +13,14 @@ export class RacesDownloadService extends ServiceBase {
     }
 
     public downloadPointPdf(raceId: string): Observable<Blob> {
-        const idHeader = [{ key: 'raceId', value: raceId }];
         return this.http.get(`${this.baseUrl}/getpointspdf`, { params: { raceId: raceId } , responseType: 'blob' });
     }
 
     public downloadTeamsPdf(raceId: string): Observable<Blob> {
-        const idHeader = [{ key: 'raceId', value: raceId }];
         return this.http.get(`${this.baseUrl}/getteamspdf`, { params: { raceId: raceId } , responseType: 'blob' });
     }
 
     public downloadStagesAndRaceEndPdf(raceId: string): Observable<Blob> {
-        const idHeader = [{ key: 'raceId', value: raceId }];
         return this.http.get(`${this.baseUrl}/getstagesandraceendpdf`, { params: { raceId: raceId } , responseType: 'blob' });
     }
 }
