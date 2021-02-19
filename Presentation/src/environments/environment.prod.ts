df6f7e359c27c4beb53f0c9fe289f3ca6ef588e7
Marco Trapman <marco.trapman@capgemini.com>
2021-02-19 14:51:03 +0100

    Fixed merge issue

--- Presentation/src/environments/environment.prod.ts
+++ Presentation/src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  apiBaseUrl: 'https://raceventuradev.azurewebsites.net'
-=======
   apiBaseUrl: 'https://raceventura.westeurope.cloudapp.azure.com'
->>>>>>> dev
 };
