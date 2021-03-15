0caee6dd6998e6838c091431a4ac3e273ea208c9
Marco Trapman <marco.trapman@capgemini.com>
2021-03-15 15:45:45 +0100

    Updated version number

--- RaceVenturaAPI/Controllers/VersionController.cs
+++ RaceVenturaAPI/Controllers/VersionController.cs
@@ -23,7 +23,7 @@ namespace RaceVenturaAPI.Controllers
         {
             try
             {
-                return Ok("1.0.7.0");
+                return Ok("1.0.8.0");
             }
             catch (Exception ex)
             {
